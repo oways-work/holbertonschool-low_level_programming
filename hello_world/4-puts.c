ff6cf9e427916ba509854ba223bc85f57dd601ea
oways-work <owayswork@gmail.com>
2025-08-28 21:53:40 +0000

    ready_from_note

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- *fist time betty style documenting
- *
- *Return : Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"programing is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
