ba1d907ad2ede4a7620fc2e1041d8d9d7f3d8f17
oways-work <owayswork@gmail.com>
2025-08-28 22:18:18 +0000

    ready_from_note

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints a specific sentence with proper grammar
+ * main - Prints a sentence with proper grammar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
