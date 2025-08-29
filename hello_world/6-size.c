8a4ec1f75d9fd70ca7e4dff30341b4d1b3b9e7fd
oways-work <owayswork@gmail.com>
2025-08-29 00:25:42 +0000

    ready_from_note

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
- /**
+/**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-  printf("size of a char: %zu byte(s)\n", sizeof(char));
-    printf("size of an int: %zu byte(s)\n", sizeof(int));
-    printf("size of a long int: %zu byte(s)\n", sizeof(long int));
-    printf("size of a float: %zu byte(s)\n", sizeof(float));
-    return (0);
+  /* The format specifier is changed to %lu */
+  /* The result of sizeof() is cast to (unsigned long) */
+
+  printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(char));
+  printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(int));
+  printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
+  
+  /* It's common for these exercises to also require long long int for 64-bit systems */
+  printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(long long int));
+  
+  printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
+  
+  return (0);
 }
