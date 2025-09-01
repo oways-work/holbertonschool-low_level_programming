218b468eac435753ffed236842dc6dfb2eeb5392
oways-work <owayswork@gmail.com>
2025-09-01 11:23:57 +0000

    ready_from_note

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,29 @@
-#include
+#include <stdio.h>
 
+/**
+ * main - Prints the lowercase alphabet and then the uppercase alphabet.
+ *
+ * This program iterates through the lowercase characters 'a' to 'z'
+ * and prints each one using putchar. It then does the same for the
+ * uppercase characters 'A' to 'Z', and finally prints a newline.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
