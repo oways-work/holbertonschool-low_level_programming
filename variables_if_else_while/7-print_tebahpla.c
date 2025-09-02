46bffb9867e13ec6f227d2e2aaacf1e18ea5eea5
root <owayswork@gmail.com>
2025-09-02 06:21:21 +0000

    DD

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point
+ *
+ * Description: this program prints lowercase alphabet
+ * but in reverse
+ *
+ * Return: always 0 (Success).
+ */
+int main(void)
+  char c;
+
+  for (c = 'z'; c <= 'a' ; c++)
+    {
+      puchar(c)
+    }
