182b98097a489043facd5cea4f7ad9beeb141568
oways-work <owayswork@gmail.com>
2025-09-03 06:18:29 +0300

    Update 7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -10,14 +10,14 @@
  */
 int main(void)
 {
-  char c;
+    char c;
 
-  for (c = 'z'; c <= 'a' ; c++)
+    for (c = 'z'; c >= 'a' ; c--)
     {
-      putchar(c);
+        putchar(c);
     }
 {
-  putchar('\n');
+   putchar('\n');
     }
   return(0);
     }
