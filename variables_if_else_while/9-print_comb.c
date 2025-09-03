5543f5643a80833f775d81546cfbcb08f3fd218f
oways-work <owayswork@gmail.com>
2025-09-03 04:28:31 +0000

    ready_from_note

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,17 +7,19 @@
  */
 int main(void)
 {
-  int n;
-  
-  for (n = 0; n <= 9; n++)
-  {
-	putchar(n + '0');
-	
-	if (n < 9)
-	  {
-	    putchar(',');
-	    putchar(' ');
-	   }
-	
-  return (0);
-  }
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
