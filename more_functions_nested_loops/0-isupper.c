943defe9ce3bae37d576a3f43745e48617db4fae
oways-work <owayswork@gmail.com>
2025-09-04 15:31:04 +0000

    ready_from_note

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - check the code.
+ * main -  Checks for an uppercase character.
+ * upprercase only
  *
  * Return: Always 0.
  */
 int _isupper(int c)
 {
-  if (c >= 'A' && c <= 'Z')
+    if (c >= 'A' && c <= 'Z')
     {
-           return (1);
+         return (1);
     }
     else
     {   
-           return (0);
+         return (0);
     }
 }
