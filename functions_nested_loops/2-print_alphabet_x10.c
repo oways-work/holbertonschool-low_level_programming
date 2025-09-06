749d5fe2a1ec2f8a24c5c880a90be2a736d6b616
oways-work <owayswork@gmail.com>
2025-09-06 16:55:59 +0000

    ready_from_note

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -14,13 +14,13 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char letter;
 
-    while (count < 10)
-    {    
+	while (count < 10)
+	{	
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
 	}
 	_putchar('\n');
 	count++;
-    }
+	}
 }
