88aca870e36c9fd744136efcee7efd430b525daa
oways-work <owayswork@gmail.com>
2025-09-06 15:08:32 +0000

    ready_from_note

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,18 +3,18 @@
 /**
  * main - Prints the string "_putchar" followed by a new line.
  *
- * Return: 1 if c is uppercase, 0 otherwise.
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
-  char *s = "_putchar";
-  
-  while (*s)
-    {
-      _putchar(*s);
-      s++;
-    }
-  _putchar('\n');
+	char *s = "_putchar";
 
-  return (0);
+	while (*s)
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
