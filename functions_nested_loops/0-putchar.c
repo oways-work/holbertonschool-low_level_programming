723b745120bb717f5ccc74884b3759fcc2778eca
oways-work <owayswork@gmail.com>
2025-09-06 15:01:41 +0000

    ready_from_note

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints the string "_putchar" followed by a new line.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+int main(void)
+{
+  char *s = "_putchar";
+
+  while (*s)
+    {
+      _putchar(*s);
+       s++
+    }
+  _putchar('\n');
+
+  return (0);
+}
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef main_h
+
+/*
+ * File: main.h
+ * Auth: OIS
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0-putchar.c project and many more.
+ */
+int _putchar(char c);
+
+#endif /*main_h */
+
