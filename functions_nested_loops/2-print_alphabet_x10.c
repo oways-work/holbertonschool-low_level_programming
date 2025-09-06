e05370b4f947f6a8f2a86fdd2d25a90b6af289cf
oways-work <owayswork@gmail.com>
2025-09-06 16:43:59 +0000

    ready_from_note

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lowercase alphabet.
+ *
+ * This function prints the lowercase alphabet from 'a' to 'z'
+ * followed by a new line.
+ * 10x times
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+    while (count < 10)
+    {    
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+	count++;
+    }
+}
