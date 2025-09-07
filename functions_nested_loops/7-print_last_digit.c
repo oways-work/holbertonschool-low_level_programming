632d307178403c5e49f2b608dd00721e4aba062a
oways-work <owayswork@gmail.com>
2025-09-07 12:06:09 +0000

    ready_from_note

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -12,11 +12,10 @@ int print_last_digit(int n)
 
 	if (last_digit < 0)
 	{
-		last_digit = -last_digit;
+	    last_digit = -last_digit;
 	}
-	
+
 	_putchar('0' + last_digit);
-	
+
 	return (last_digit);
-}
-			
+}			
