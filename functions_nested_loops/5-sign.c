ec75430abfe0589e1ad0a4a554a39c7c10a42275
oways-work <owayswork@gmail.com>
2025-09-07 10:14:26 +0000

    ready_from_note

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,32 +1,32 @@
 #include "main.h"
 
 /**
- * main - check the code.
- * 
- * Return: Always 0.
+ * print_sign - prints the sign of a number
+ * @n: The number to check
+ *
+ * Description: This function checks the sign of a number 'n'.
+ * It prints a character ('+', '0', or '-') based on the sign and returns
+ * a corresponding integer value.
+ *
+ * Return: 1 and prints '+' if n is greater than zero,
+ * 0 and prints '0' if n is zero,
+ * -1 and prints '-' if n is less than zero.
  */
-int _isalpha(int n)
-{  
-  int = n;
-
-  if (n >= 0)
-    {
-      _putchar('+')
-	
-      return (1);
-      
-    }
-  if (n == '0')
-    {
-      putchar(0);
-      
-      return (0);
-      
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
 	}
-  if (n < '0');
-  {
-    _putchar('-');
-    
-    return (-1);
-  }
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
