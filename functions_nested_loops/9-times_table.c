befb00d72e7023a8d3ec052d30a99d930c7beeaa
oways-work <owayswork@gmail.com>
2025-09-11 13:45:57 +0000

    ready_from_note

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,39 +6,37 @@
  * Description: This function prints a formatted 9x9 multiplication table.
  * Each row is on a new line, and numbers are separated by a comma
  * and a space. Single-digit numbers are padded with an extra space.
- * Return: void
  */
 void times_table(void)
 {
-    int row, col, product;
+	int row, col, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (col = 0; col <= 9; col++)
-        {
-            product = row * col;
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			product = row * col;
+			if (col == 0)
+			{
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 
-            if (col == 0)
-            {
-                _putchar('0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-
-                if (product < 10)
-                {
-                    _putchar(' ');
-                    _putchar(product + '0');
-                }
-                else
-                {
-                    _putchar((product / 10) + '0');
-                    _putchar((product % 10) + '0');
-                }
-            }
-        }
-        _putchar('\n');
-    }
+				if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				else
+				{
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
