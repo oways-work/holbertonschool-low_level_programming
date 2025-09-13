13cbdf088676c978c83d63c3a9e5ef2f402641dd
oways-work <owayswork@gmail.com>
2025-09-13 14:14:52 +0000

    ready_from_note

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef main_h
+
+/*
+ * File: main.h
+ * Auth: OIS
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0-putchar.c project and many more.
+ */
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int print_sign(int n);
+int _putchar(char c);
+void print_alphabet(void);
+int _islower(int c);
+void print_alphabet_x10(void);
+int _isalpha(int c);
+int _abs(int);
+char *_memset(char *s, char b, unsigned int n);
+#endif /*main_h */
