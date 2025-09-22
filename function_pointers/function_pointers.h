83374c84826e89e092b1a9931f6c68331f6f93d5
oways-work <owayswork@gmail.com>
2025-09-22 16:30:42 +0000

    this was easy

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -7,4 +7,7 @@ int _putchar(char c);
 /* Prototype for Task 0 */
 void print_name(char *name, void (*f)(char *));
 
+/*task 1*/
+void array_iterator(int *array, size_t, void, (*action)(int)
+		    
 #endif /* FUNCTION_POINTERS_H */
