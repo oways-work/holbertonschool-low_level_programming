308305543b1c031bf17837c0834b6cfab72dd0a9
oways-work <owayswork@gmail.com>
2025-09-22 17:38:46 +0000

    ready_from_note

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#inlude "function_pointers.h"
+/**
+ * is_98 - check if a number is equal to 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int int_index(int *array, size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return(-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) !=0))
+		{
+			return(i);
+		}
+	}
+	return(-1)
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -12,4 +12,6 @@ void print_name(char *name, void (*f)(char *));
 /* Prototype for Task 1 */
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+/*prototype for task 2 */
+int int_index(int *array, int size, int (*cmp)(int));
 #endif /* FUNCTION_POINTERS_H */
