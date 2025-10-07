912be1c53f7e3a797c9c13afaa70f0a09ea694b8
oways-work <owayswork@gmail.com>
2025-10-07 09:41:14 +0000

    ready_from_note

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -15,9 +15,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+\tchar *str;
+\tunsigned int len;
+\tstruct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
