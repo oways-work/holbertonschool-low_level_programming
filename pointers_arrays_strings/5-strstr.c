7264201da3c4737da20e0e3a17ad7920381c1a33
oways-work <owayswork@gmail.com>
2025-10-12 13:42:15 +0300

    Create 5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring.
+ * @haystack: The string to be searched.
+ * @needle: The substring to find.
+ *
+ * Return: A pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
