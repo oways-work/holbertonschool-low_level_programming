20ea11972f40311e4197b244deb8269ee80ec3d5
oways-work <owayswork@gmail.com>
2025-10-13 02:40:00 +0300

    Update lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h> /* For size_t */
+#include <stdio.h>  /* For printf, as allowed by Task 0 */
+#include <stdlib.h> /* For malloc, free, exit, and strdup (as allowed by Tasks 2, 3, 4) */
 
 /**
  * struct list_s - singly linked list
@@ -15,15 +15,63 @@
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
+/* Helper prototype for string length calculation (used by add_node and add_node_end) */
+unsigned int _strlen(const char *s);
 
+/* Function prototypes */
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: A pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
 size_t print_list(const list_t *h);
 
-/* Add prototypes for your other functions here as you write them */
+/**
+ * list_len - Returns the number of elements in a linked list_t list.
+ * @h: A pointer to the head of the list.
+ *
+ * Return: The number of nodes.
+ */
+size_t list_len(const list_t *h);
 
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: A pointer to a pointer to the head of the list.
+ * @str: The string to be duplicated and stored in the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
+list_t *add_node(list_t **head, const char *str);
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: A pointer to a pointer to the head of the list.
+ * @str: The string to be duplicated and stored in the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
+list_t *add_node_end(list_t **head, const char *str);
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the head of the list.
+ */
+void free_list(list_t *head);
 
 #endif /* LISTS_H */
