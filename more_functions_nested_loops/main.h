cc633a83464eb825c95b4e0e0312dd402c3c980a
oways-work <owayswork@gmail.com>
2025-10-12 09:24:30 +0300

    Update main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,2 +1,12 @@
+#ifndef main_h
+
+/*
+ * File: main.h
+ * Auth: OIS
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0-putchar.c project and many more.
+ 
+#endif /*main_h */
+
 int _isupper(int c);
 int _isdigit(int c);
