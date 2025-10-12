c335b512af18189a4733c7c6b4bda1dc8a9eb6b4
oways-work <owayswork@gmail.com>
2025-10-12 04:55:07 +0000

    hi

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -3,9 +3,7 @@
 /**
  * times_table - Prints the 9 times table, starting with 0.
  *
- * Description: This function prints a formatted 9x9 multiplication table.
- * Each row is on a new line, and numbers are separated by a comma
- * and a space. Single-digit numbers are padded with an extra space.
+ * Return: Nothing.
  */
 void times_table(void)
 {
