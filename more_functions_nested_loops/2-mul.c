0cd416be49d2b5b1e2568ccb0344356d3909728e
oways-work <owayswork@gmail.com>
2025-10-12 09:49:23 +0300

    Update 2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * mul is a function that multiplies two integers
  *
- * Return: Always 0.
+ *
+ * It returns the result after the operation is done
  */
 int mul(int a, int b)
 {
