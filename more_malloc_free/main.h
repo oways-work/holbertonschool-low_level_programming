3d3ee152070c3aa3bfb3baf12f6cb53e41b62c86
oways-work <owayswork@gmail.com>
2025-10-12 13:12:08 +0300

    Create main.h

---
+++ more_malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+int _putchar(char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+void *malloc_checked(unsigned int b);
+
+#endif /* MAIN_H */
+
