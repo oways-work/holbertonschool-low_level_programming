19ff937e391ec0245e31e89e98b80aba38b2eb9d
oways-work <owayswork@gmail.com>
2025-10-12 13:46:18 +0300

    Update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,21 +1,15 @@
-#ifndef main_h
+#ifndef MAIN_H
+#define MAIN_H
 
-/*
- * File: main.h
- * Auth: OIS
- * Desc: Header file containing prototypes for all functions
- * used in the 0-putchar.c project and many more.
- */
-int add(int, int);
-void times_table(void);
-void jack_bauer(void);
-int print_last_digit(int);
-int print_sign(int n);
 int _putchar(char c);
-void print_alphabet(void);
-int _islower(int c);
-void print_alphabet_x10(void);
-int _isalpha(int c);
-int _abs(int);
 char *_memset(char *s, char b, unsigned int n);
-#endif /*main_h */
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
+#endif /* MAIN_H */
+
