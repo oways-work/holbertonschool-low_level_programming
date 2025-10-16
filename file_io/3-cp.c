bd4c673994d6361d6186d0c54d7d0d6af953e711
oways-work <owayswork@gmail.com>
2025-10-16 09:52:05 +0300

    Update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -26,7 +26,7 @@ void close_fd(int fd)
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
+	ssize_t bytes_read;
 	char buffer[1024];
 	mode_t file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
@@ -35,12 +35,14 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, file_perm);
 	if (fd_to == -1)
 	{
@@ -48,10 +50,21 @@ int main(int argc, char *argv[])
 		close_fd(fd_from);
 		exit(99);
 	}
-	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+
+	while (1)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
+		bytes_read = read(fd_from, buffer, 1024);
+		if (bytes_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close_fd(fd_from);
+			close_fd(fd_to);
+			exit(98);
+		}
+		if (bytes_read == 0) /* End of file */
+			break;
+
+		if (write(fd_to, buffer, bytes_read) != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close_fd(fd_from);
@@ -59,13 +72,7 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close_fd(fd_from);
-		close_fd(fd_to);
-		exit(98);
-	}
+
 	close_fd(fd_from);
 	close_fd(fd_to);
 	return (0);
