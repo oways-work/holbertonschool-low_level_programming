20ed27b9a1bee176c1a82f9663f252b1654cd721
oways-work <owayswork@gmail.com>
2025-10-16 09:54:39 +0300

    Update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,9 +3,6 @@
 /**
  * close_fd - Closes a file descriptor and handles potential errors.
  * @fd: The file descriptor to be closed.
- *
- * Description: If closing the file descriptor fails, it prints an error
- * message to the standard error and exits with code 100.
  */
 void close_fd(int fd)
 {
@@ -35,14 +32,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, file_perm);
 	if (fd_to == -1)
 	{
@@ -50,20 +45,8 @@ int main(int argc, char *argv[])
 		close_fd(fd_from);
 		exit(99);
 	}
-
-	while (1)
+	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
-		bytes_read = read(fd_from, buffer, 1024);
-		if (bytes_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			close_fd(fd_from);
-			close_fd(fd_to);
-			exit(98);
-		}
-		if (bytes_read == 0) /* End of file */
-			break;
-
 		if (write(fd_to, buffer, bytes_read) != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -72,9 +55,33 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close_fd(fd_from);
+		close_fd(fd_to);
+		exit(98);
+	}
 	close_fd(fd_from);
 	close_fd(fd_to);
 	return (0);
+}
+```
+
+### Explanation of the Fix
+
+The core of the logic is in this structure:
+
+```c
+// 1. The loop continues as long as read() is successful and returns bytes (> 0)
+while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+{
+    // ... attempt to write ...
+}
+
+// 2. This check happens IMMEDIATELY after the loop terminates.
+if (bytes_read == -1)
+{
+    // ... handle the read error and exit with code 98 ...
 }
 
