fbc22b4b201060ce68e893de689022ea3b236859
oways-work <owayswork@gmail.com>
2025-11-02 03:38:46 +0300

    Update main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef main_h
+#define main_h
 
 /*
  * File: main.h
@@ -14,5 +15,10 @@ void print_numbers(void);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
 #endif /*main_h */
+
