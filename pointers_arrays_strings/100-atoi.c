b0664e7a93ff88c13add76cc4c6ff20cbe077aba
oways-work <owayswork@gmail.com>
2025-11-13 10:28:48 +0300

    Update 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -14,7 +14,8 @@ int _atoi(char *s)
 	int i = 0;
 	int sign = 1; /* 1 for positive, -1 for negative */
 	int started = 0;
-	int result = 0;
+	int result = 0; /* Accumulate as negative to handle INT_MIN */
+	int limit = -214748364; /* -INT_MAX / 10 */
 
 	while (s[i] != '\0')
 	{
@@ -28,23 +29,29 @@ int _atoi(char *s)
 		{
 			started = 1;
 
-			/* Use negative accumulation to safely handle INT_MIN */
+			if (result < limit)
+			{
+				/* Overflow imminent, return min or max */
+				if (sign == 1)
+					return (2147483647);
+				else
+					return (-2147483648);
+			}
+
+			result *= 10;
 			if (sign == 1)
 			{
-				/* Check for positive overflow before calculating next result */
-				if (result > 2147483647 / 10 || (result == 2147483647 / 10 && (s[i] - '0') > 7))
-					return (2147483647); /* Return INT_MAX (positive) */
-				
-				result = result * 10 + (s[i] - '0');
+				/* Check positive overflow condition (for 7 or more) */
+				if (result < -2147483647 + (s[i] - '0'))
+					return (2147483647);
+				result -= (s[i] - '0'); /* Subtracting a positive number */
 			}
-			else /* sign == -1 (negative) */
+			else /* sign == -1 */
 			{
-				/* Check for negative overflow before calculating next result */
-				/* (INT_MIN is -2147483648) */
-				if (result < -2147483648 / 10 || (result == -2147483648 / 10 && (s[i] - '0') > 8))
-					return (-2147483648); /* Return INT_MIN (negative) */
-
-				result = result * 10 - (s[i] - '0'); /* Subtracting to accumulate as negative */
+				/* Check negative overflow condition (for 8 or more) */
+				if (result < -2147483648 + (s[i] - '0'))
+					return (-2147483648);
+				result -= (s[i] - '0'); /* Subtracting a negative number */
 			}
 		}
 		else if (started == 1)
@@ -55,8 +62,8 @@ int _atoi(char *s)
 		i++;
 	}
 
-	/* Final result application: if accumulated as negative, use sign=1 */
-	if (sign == -1 && result > 0)
+	/* If accumulated negatively but sign is positive, negate result */
+	if (sign == 1)
 		return (-result);
 
 	return (result);
