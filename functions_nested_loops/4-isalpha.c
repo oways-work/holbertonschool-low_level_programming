1df7f8b5334d245719dbb4660025f0c2874d9744
oways-work <owayswork@gmail.com>
2025-11-13 09:41:05 +0300

    Update 4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,24 +1,16 @@
 #include "main.h"
 
 /**
- * _islower - Checks if a character is a lowercase letter.
- * @c: The character to check.
+ * _isalpha - checks for alphabetic character
+ * @c: The character to be checked
  *
- * This function takes an integer 'c' which represents a character
- * from the ASCII table. It uses a conditional statement to determine
- * if the character's ASCII value falls within the range of lowercase or uppercase
- * letters ('a' to 'z') or ('A' to 'Z')
- *
- * Return: 1 if the character is lowercase or uppercase, 0 otherwise.
+ * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise
  */
 int _isalpha(int c)
 {
-  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
