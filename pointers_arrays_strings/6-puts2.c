511832275a774982604a3db7a8b090e96baa3173
oways-work <owayswork@gmail.com>
2025-11-13 10:18:52 +0300

    Create 6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string, starting with the first.
+ * @str: The string to be processed.
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
